bea4345d1e5a730d63b8f222aaecc506b8fc5d0b
jwong0123 <64813485+jwong0123@users.noreply.github.com>
2020-11-01 16:56:50 -0500

    Update Xor.java

--- Xor.java
+++ Xor.java
@@ -1,37 +1,59 @@
 public class Xor {
-
     public static String compare(String key, String message) {
-        /*
-         * String ky = ""; String x = ""; Integer i; Integer j; if (key.length() <
-         * message.length()) { i = message.length() / key.length(); j = message.length()
-         * % key.length(); } while (i > 0) ky.append(ky, key); ky.append(ky,
-         * key.substring(0, j)); System.out.println(ky);
-         * 
-         * for (int i = 0; i < message.length(); i++) { x.append((char)
-         * (message.charAt(i) ^ ky.charAt(i))); String result = x.toString(); } return
-         * result;
-         */
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < message.length(); i++)
             sb.append((char) (message.charAt(i) ^ key.charAt(i % key.length())));
         String result = sb.toString();
         return result;
     }
-    private String store(String key, String message)
+
+    public static String numOut(String x) {
+        String hex = "";
+        for (int i = 0; i < x.length(); i++){
+            char ch = x.charAt(i);
+            int in = (int)ch;
+            String part = Integer.toHexString(in);
+            hex += part + " ";
+        }
+        return hex;
+    }
+
+    private static String subk(String key) {
+        String key1 = "A";
+        String key2 = "FISH";
+        String ky = "";
+        if (key.equals("key1"))
+            ky = key1;
+        else
+            ky = key2;
+        return ky;
+    }
+
+    private static String subm(String message) {
+        String message1 = "hello";
+        String message2 = ")$--.";
+        String message3 = "this is a test";
+        String message4 = "f=;l5i:;f(s<#:";
+        String ms = "";
+        if (message.equals("message1"))
+            ms = message1;
+        else if (message.equals("message2"))
+            ms = message2;
+        else if (message.equals("message3"))
+            ms = message3;
+        else
+            ms = message4;
+        return ms;
+    }
 
     public static void main(String[] args) {
-        
-    String key1 = "A";
-    String key2 = "FISH";
-    String message1 = "hello";
-    String message2 =")$--.";
-    String message3 = "this is a test";
-    String message4 = "f=;l5i:;f(s<#"
-    //String mode = (args[0]); 
-    String key = (args[1]); 
-    String message = (arg[2]);
-    
-        System.out.println(compare("FISH", " this is a test"));
-
+        String mode = (args[0]);
+        String key = (args[1]);
+        String message = (args[2]);
+        String m = compare(subk(key), subm(message));
+        if (mode.equals("human"))
+            System.out.println(m);
+        else
+            System.out.println(numOut(m));
     }
 }
